252fe1e5cd0b64c89854ee56fbbe75439557b0d9
OfficialOkanlawon <okanlawonimmanuel@gmail.com>
2022-04-07 14:13:41 -0700

    C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,12 @@
 */
 int main(void)
 {
+
 		/*initialize string*/
 		char Mystr[] = "\"Programming is like building a multilingual puzzle";
 			/*writing string to stdout*/
 			puts(Mystr);
+			
 			return (0);
+
 }			
