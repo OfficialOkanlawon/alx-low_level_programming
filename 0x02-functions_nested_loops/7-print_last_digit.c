cf2ef49a63550438d05e78a3f3ce3059783dc4ed
Okanlawon Oluwatobi <66867446+OfficialOkanlawon@users.noreply.github.com>
2022-04-13 23:41:58 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,51 +1,26 @@
 #include "main.h"
 
-
-
 /**
+ * print_last_digit - Prints the last digit
+ * of a number.
  *
- *  * print_last_digit - Prints the last digit
+ * @n: input number as an integer.
  *
- *   * of a number.
- *
- *    *
- *
- *     * @n: input number as an integer.
- *
- *      *
- *
- *       * Return: last digit.
- *
- *        */
-
+ * Return: last digit.
+ */
 int print_last_digit(int n)
-
 {
+	int l;
 
-		int l;
-
-
-
-			l = n % 10;
-
-				if (l < 0)
-
-						{
-
-									_putchar(-l + 48);
-
-											return (-l);
-
-												}
-
-					else
-
-							{
-
-										_putchar(l + 48);
-
-												return (l);
-
-													}
-
+	l = n % 10;
+	if (l < 0)
+	{
+		_putchar(-l + 48);
+		return (-l);
+	}
+	else
+	{
+		_putchar(l + 48);
+		return (l);
+	}
 }
