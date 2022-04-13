ef5ba73040ed492aed2555a9d3f481747f9f799e
Okanlawon Oluwatobi <66867446+OfficialOkanlawon@users.noreply.github.com>
2022-04-13 17:12:04 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,51 +1,20 @@
 #include "main.h"
 
-
-
 /**
+ * print_alphabet_x10 - Prints the alphabet 10 times.
  *
- *  * print_alphabet_x10 - prints alphabets 10
- *
- *   *
- *
- *    * Description: prints 10 times
- *
- *     * Return: Always(0).
- *
- *      */
-
-
-
+ * Return: Always 0 (Success)
+ */
 void print_alphabet_x10(void)
-
 {
+	int a, b;
 
-		int alphabet_count = 0;
-
-
-
-			while (alphabet_count < 10)
-
-					{
-
-								char ch = 'a';
-
-
-
-										while (ch <= 'z')
-
-													{
-
-																	_putchar(ch);
-
-																				ch++;
-
-																						}
-
-												alphabet_count++;
-
-														_putchar('\n');
-
-															}
-
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 97; b <= 122; b++)
+		{
+			_putchar(b);
+		}
+	_putchar('\n');
+	}
 }
