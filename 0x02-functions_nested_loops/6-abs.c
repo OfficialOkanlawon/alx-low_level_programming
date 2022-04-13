dda0e1f1b10d0d0f2ecc2cac9079b6f44de4e943
Okanlawon Oluwatobi <66867446+OfficialOkanlawon@users.noreply.github.com>
2022-04-13 23:42:46 +0100

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,41 +1,21 @@
 #include "main.h"
 
-
-
 /**
+ * _abs - Computes the absolute value
+ * of an integer.
  *
- *  * _abs - Computes the absolute value
+ * @i: input number as an integer.
  *
- *   * of an integer.
- *
- *    *
- *
- *     * @i: input number as an integer.
- *
- *      *
- *
- *       * Return: absolute value
- *
- *        */
-
+ * Return: absolute value
+ */
 int _abs(int i)
-
 {
-
-		if (i >= 0)
-
-				{
-
-							return (i);
-
-								}
-
-			else
-
-					{
-
-								return (i * -1);
-
-									}
-
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
 }
